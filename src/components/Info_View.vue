f60a969533ce5fb7e43794722205ef9deff6db2f
Felix Brandt <ms861418@campussachsen.onmicrosoft.com>
2017-10-16 16:27:49 +0200

    umlaute

--- src/components/Info_View.vue
+++ src/components/Info_View.vue
@@ -13,15 +13,15 @@
 		            <h4>Ziel</h4>
 		            Unter dem Motto "Praktisch mehr erreichen" entstehen unter der dem Dach der HTW viele Projekte, die dazu dienen die Lehre in den Vorlesungen zu verbessern und den Wissensdurst der Studenten zu stillen. Dieses Projekt dient dazu Algorithmen, die in der Produktion angewandt werden zu veranschaulichen. 
 		            <h4>Wissenskompetenzen</h4>
-		            Das Projekt bietet eine umfangreiche Moeglichkeit sein Wissen zu erweitern. Studenten der Wirtschaftswissenschaften, sowie die der Informatik k�nnen sich zusammentun und gemeinsam Fragestellungen erschlie�en die in der Vorlesung zum Bereich der Produktionsplanung auftreten.
+		            Das Projekt bietet eine umfangreiche Moeglichkeit sein Wissen zu erweitern. Studenten der Wirtschaftswissenschaften, sowie die der Informatik können sich zusammentun und gemeinsam Fragestellungen erschließen die in der Vorlesung zum Bereich der Produktionsplanung auftreten.
 			            <div class="showcase-top text-center">
 					  	 <img src="../assets/Info_1.png"> <p class="caption">
 					     Vielfalt an abgedeckten Themengebieten      
 					    </p>
 					    </div>		
 					
-					Aber was f�r Potential steckt noch dahinter? Die Oberfl�che gef�llt nicht ganz? GUI und Design-Entwicklung k�nnen in diesem Projekt ge�bt und erforscht werden. Wer sich eher mit Algorithmen anfreunden m�chte, darf sich an verschieden Backendsysteme wagen und diese als Microservice verf�gbar machen.             
-				    Ob Java,C# oder Golang euch sind keine Grenzen gesetzt. Teamf�higkeiten k�nnen gest�rkt werden oder die Projektplanung �berwacht.
+					Aber was für Potential steckt noch dahinter? Die Oberfläche gefällt nicht ganz? GUI und Design-Entwicklung können in diesem Projekt geübt und erforscht werden. Wer sich eher mit Algorithmen anfreunden möchte, darf sich an verschieden Backendsysteme wagen und diese als Microservice verfügbar machen.
+				    Ob Java,C# oder Golang euch sind keine Grenzen gesetzt. Teamfähigkeiten können gestärkt werden oder die Projektplanung überwacht.
 		            <h4>Interesse?</h4>
 		            
 		            Eure Ansprechpartner fuer dieses Projekt:
